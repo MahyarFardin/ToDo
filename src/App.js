e10f0eb58886a0c1267367b0152b3511dd7e7d6a
mahyarfardin <mahyarfardinfar@gmail.com>
2022-04-12 16:08:18 +0430

    done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 //Components
 import Inputs from './Component/Inputs/Inputs.component';
@@ -10,13 +10,32 @@ const App = () => {
   const [inBoxText, setInBoxText] = useState("");
   const [toDoJobs, setToDoJobs] = useState([]);
   const [filterType, setFilterType] = useState("All");
+  const [filteredItems, setFilteredItems] = useState([]);
+
+  //Filtering my list
+  const filterHandeler = () => {
+    if (filterType === "All") {
+      setFilteredItems(toDoJobs);
+    }
+    else if (filterType === "Completed") {
+      setFilteredItems(toDoJobs.filter((todo) => (todo.completed === true)));
+    }
+    else {
+      setFilteredItems(toDoJobs.filter((todo) => (todo.completed === false)));
+    }
+  };
+
+  // call filter handeler when items chainge
+  useEffect(() => {
+    filterHandeler();
+  }, [toDoJobs, filterType]);
 
   //Return of this functions
   return (
     <div className='App'>
       <h1>ToDo App</h1>
       <Inputs setFilterType={setFilterType} toDoJobs={toDoJobs} inBoxText={inBoxText} setInBoxText={setInBoxText} setToDoJobs={setToDoJobs} />
-      <ToDoList setToDoJobs={setToDoJobs} toDoJobs={toDoJobs}/>
+      <ToDoList filteredItems={filteredItems} setToDoJobs={setToDoJobs} toDoJobs={toDoJobs} />
     </div>
   );
 }
--- src/Component/List/ToDoList.component.jsx
+++ src/Component/List/ToDoList.component.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Jobs from "../Jobs/Jobs.component";
 import "./ToDoList.style.css"
 
-const ToDoList = ({toDoJobs, setToDoJobs, setFilterType}) => (
+const ToDoList = ({toDoJobs, setToDoJobs, filteredItems}) => (
     <div className="todo-container">
         <ul className="todo-list">
-            {toDoJobs.map((toDoJob)=>(<Jobs text={toDoJob.text}  setToDoJobs={setToDoJobs} todos={toDoJobs} todo={toDoJob} key={toDoJob.id}/>))}
+            {filteredItems.map((toDoJob)=>(<Jobs text={toDoJob.text}  setToDoJobs={setToDoJobs} todos={toDoJobs} todo={toDoJob} key={toDoJob.id}/>))}
         </ul>
     </div>
 )
